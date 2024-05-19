86f9218c98bf2056a419f7134bde1e6c8241735e
Farmerbilly27 <dmw2796@gmail.com>
2024-05-19 15:04:27 -0400

    change icons, add transportation schedule

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -3,48 +3,61 @@ import TopBar from "./TopBar";
 import { Stack } from "@mui/system";
 import { Divider, Fade, Typography, ThemeProvider, createTheme, Box, Link, Grid, Card, CardContent, Container, Paper, List, ListItem, ListItemText } from "@mui/material";
 import { Fonts } from "../utils/Fonts";
-import WineBarIcon from '@mui/icons-material/WineBar';
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import { FaBus } from "react-icons/fa";
 import CelebrationIcon from '@mui/icons-material/Celebration';
+import { GiLinkedRings } from "react-icons/gi";
+import { CiApple } from "react-icons/ci";
+import { PiCheersLight } from "react-icons/pi";
 
 interface Event { 
   title: string, 
-  time: string, 
-  description: string,
-  address?: string,
+  time?: string, 
+  description?: string,
+  descriptionCss?: { [key: string]: string },
+  addresses?: string[],
   icon?: any,
 };
- 
+
 const satEvents: Event[] = [
-  { title: 'Welcome Event at Bold Rock Carter Mountain', icon: <WineBarIcon />, time: '2:00 - 4:00 pm', description: 'Join us at Bold Rock Carter Mountain cidery for drinks and apple cider donuts!', address: '1435 Carters Mountain Trail, Charlottesville, VA 22902' },
+  { title: 'Welcome Event at Bold Rock Carter Mountain', icon: <CiApple />, time: '2:00 - 4:00 pm', description: 'Join us at Bold Rock Carter Mountain cidery for drinks and apple cider donuts!', addresses: ['1435 Carters Mountain Trail, Charlottesville, VA 22902'] },
 ];
 const sunEvents: Event[] = [
-  { title: 'Wedding Ceremony', icon: <FavoriteBorderIcon />, time: '5:00 pm', description: 'Ceremony will be at the The Market at Grelen.', address: '15091 Yager Rd, Somerset, VA 22972' },
-  { title: 'Wedding Reception', icon: <CelebrationIcon />, time: '7:00 pm', description: 'Reception will follow after cocktail hour at the Market.' },
+  { title: 'Transportation to Wedding', icon: <FaBus />, time: '4:00 pm', description: 'Busses will leave promptly at 4:05, if you miss the bus you will need to find your own transportation to the venue.', descriptionCss: { textDecoration: 'underline' }, addresses: ['212 Ridge McIntire Rd, Charlottesville, VA 22903'] },
+  { title: 'From OMNI', description: 'Pickup for guests staying at the Omni Hotel is at the Water Street Bus pickup location just outside of the hotel.', addresses: ['212 Ridge McIntire Rd, Charlottesville, VA 22903'] },
+  { title: 'From Home2Suites', description: 'Pickup for guests staying at the Home2Suites will be at the hotel.', addresses: ['201 Monticello Ave, Charlottesville, VA 22902'] },
+  { title: 'Wedding Ceremony', icon: <GiLinkedRings />, time: '5:00 pm', description: 'Ceremony will be at the The Market at Grelen.', addresses: ['15091 Yager Rd, Somerset, VA 22972'] },
+  { title: 'Wedding Reception', icon: <PiCheersLight />, time: '7:00 pm', description: 'Reception will follow after cocktail hour at the Market.' },
 ]
 
 function genGridItemsForDay(events: Event[]) {
   return (events.map((event, index) => (
     <Grid container justifyContent="space-between" sx={{ marginLeft: { xs: '1rem', md: '4rem' }, marginRight: { xs: '0.5rem', md: '2rem' } }}>
       <Grid item xs={2}>
-        <Typography variant="body1" sx={{ textAlign: 'left', fontSize: { xs: '1rem', md: '1.5rem'} }}>
-          {event.time}
-        </Typography>
+        {event.time && 
+          <Typography variant="body1" sx={{ textAlign: 'left', fontSize: { xs: '1rem', md: '1.5rem'} }}>
+            {event.time}
+          </Typography>
+        }
       </Grid>
       <Grid item xs={1}>
         <Divider orientation="vertical" sx={{ width: '10px', height: '100%' }}/>
       </Grid>
       <Grid item xs={9} sx={{ marginBottom: '3rem' }}>
         <Typography variant="body1" sx={{ textAlign: 'left', fontSize: { xs: '1rem', md: '1.5rem'} }}>
-          <strong>{event.title} </strong><span style={{ verticalAlign: 'middle' }}>{event.icon}</span>
+          <strong>{event.title} </strong>
+          { event.icon && <span style={{ verticalAlign: 'middle' }}>{event.icon}</span> }
           <br />
           <Container sx={{ fontSize: { xs: '0.85rem', md: '1.4rem'}, paddingLeft: '0 !important' }}>
-            <a href={`https://www.google.com/maps/search/?api=1&query=${event.address}`} target="_blank" rel="noreferrer">
-              {event.address}
-            </a>
+            {
+              event.addresses?.map((address) => (
+                <a href={`https://www.google.com/maps/search/?api=1&query=${address}`} target="_blank" rel="noreferrer">
+                  {address}
+                </a>
+              ))
+            }
           </Container>
           <br />
-          {event.description}
+          <div style={{ ...event.descriptionCss }}>{event.description}</div>
         </Typography>
       </Grid>
     </Grid>
